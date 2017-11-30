dbc96ab6b4085103c5bb9ac31803395aeafd9477
Jacqueline Lee <anginblu@gmail.com>
2017-11-30 19:16:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,9 @@ require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/paramable'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
   extend Findable
   include Paramable
 
